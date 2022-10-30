1060c0f73be2492305d0a0793ab4badb64e19caf
EnterpriseODS <damikhalakioaye@gmail.com>
2022-10-30 20:29:53 +0200

    home work #3 practical lesson 2

---
+++ rows_and_numbers_advance.py
@@ -0,0 +1,4 @@
+name = input("Hi! Please enter you name ")
+result = name.strip().capitalize()
+print(f"Nice to meat you {result}")
+print("length of string --> ", len(result), "\n", "backwards name -->", (result[::-1]))
