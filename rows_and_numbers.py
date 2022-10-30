cfcab5e23f16127de21685be1d41341b7e231ad9
EnterpriseODS <damikhalakioaye@gmail.com>
2022-10-30 18:45:32 +0200

    home work #3 Strings, Numbers

---
+++ rows_and_numbers.py
@@ -0,0 +1,35 @@
+username = input('Please enter your name ')
+result = f"Hi {username}, nise to meet you"
+print(result)
+pass
+
+# you number
+number = input("Enter you Number ")
+result = F"your number is {number}"
+print(result)
+pass
+
+# integer
+number = int(input("enter you integer "))
+result = F"your integer is {number}"
+print(result)
+pass
+
+# 4th power of an integer
+number = int(input("enter number to exponential "))
+result = int(number) **4
+print(f"{number} to the 4th power equals: {result}")
+pass
+
+# The root of an integer
+number = float(input("enter the number from which you want to take the square root "))
+result = int(number) ** 0.5
+print(f"the square root of {number} is {result}")
+pass
+
+# The remainder from dividing an integer by 2
+number = float(input("enter a number to divide "))
+result = int(number) % 2
+print(f"The remainder of dividing the number {number} by 2 is {result}")
+pass
+print("Thank you! We've got it all!")
