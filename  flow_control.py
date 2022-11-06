156d250efcc0a3a1ed45a01e12ca8c9341ef88f2
EnterpriseODS <damikhalakioaye@gmail.com>
2022-11-06 21:29:41 +0200

    home work # 4

---  flow_control.py
+++  flow_control.py
@@ -1,8 +1,19 @@
-a = float(input("enter the first number "))
-operation = input("choice operations: (* ; / ; - ; + )")
-b = float(input("enter the second number "))
+a = (input("enter the first number "))
+b = (input("enter the second number "))
+# validation and data type assignment
+try:
+    a = int(a)
+except ValueError:
+    a = float(a)
+try:
+    b = int(b)
+except ValueError:
+    b = float(b)
+
+operation = input("choice operations: (* ; / ; - ; + ) ")
 
 result = 0
+
 if operation == '+':
     result = (a + b)
 elif operation == '-':
@@ -13,5 +24,4 @@ elif operation == '/':
     result = a / b
 elif not operation in ['*' or '/' or '-' or '+']:
     print('not a correct sign')
-result = print(int(result)) if result == int else print(float(result))
-
+print(f'result: {result}')
